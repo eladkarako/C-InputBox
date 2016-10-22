e76e0d44578f3f83941a5f7ff54b48c43e6c6da0
Elad Karako <eladkarako@users.noreply.github.com>
2016-10-22 18:21:36 +0300

    initial

---
+++ inputbox/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace inputbox
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      string sPrompt = "";
+      string sTitle = "";
+      string sDefault = "";
+      
+      foreach (string arg in args) {
+        string tmp = arg.Trim();
+        
+        if (tmp.StartsWith("-prompt=", StringComparison.CurrentCultureIgnoreCase))
+          sPrompt = tmp.Replace("-prompt=", "").Replace("\"", "").Replace("\r", "").Replace("\\n", "\n").Replace("\\t", "    ");
+        else if (tmp.StartsWith("-title=", StringComparison.CurrentCultureIgnoreCase))
+          sTitle = tmp.Replace("-title=", "").Replace("\"", "").Replace("\r", "").Replace("\\n", "\n").Replace("\\t", "    ");
+        else if (tmp.StartsWith("-default=", StringComparison.CurrentCultureIgnoreCase))
+          sDefault = tmp.Replace("-default=", "").Replace("\"", "").Replace("\r", "").Replace("\\n", "\n").Replace("\\t", "    ");
+      }
+      if (sPrompt.Equals("") && sTitle.Equals("") && sDefault.Equals("")) {
+        Console.Error.WriteLine("Error: missing -prompt=\"..\" or -title=\"..\" or -default=\"..\"");
+      } else {
+        Console.WriteLine(
+          Microsoft.VisualBasic.Interaction.InputBox(sPrompt, sTitle, sDefault)
+        );
+      }
+    }
+  }
+}
---
+++ inputbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("inputbox")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("inputbox")]
+[assembly: AssemblyCopyright ("Copyright 2016")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible (true)]
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion ("1.0.*")]
+[assembly: Guid ("6dd374bb-4db4-4776-8e85-2db35b8da7d4")]
