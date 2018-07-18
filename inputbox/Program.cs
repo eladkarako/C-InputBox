9ca25d175f511e71d5d811e12110e7b033937f57
Elad Karako <eladkarako@users.noreply.github.com>
2018-07-18 05:43:25 +0300

    making StyleCop happy by removing spaces, adding file/function headers, moving stuff around and changing variable 'renaming-style'. adding explicit exit code and return value to main-method. renaming inputbox to InputBox - both files and internal-content (classes, namespace), ComVisible to false, CLSCompliant to true, signing assembly again using old 'InputBox.snk'.

--- inputbox/Program.cs
+++ inputbox/Program.cs
@@ -1,33 +1,66 @@
-﻿using System;
-using Microsoft.VisualBasic;
-
-namespace inputbox
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" author="Elad Karako" company="no">
+//     UnLicensed (Public Domain).
+// </copyright>
+//-----------------------------------------------------------------------
+namespace InputBox
 {
-  class Program
+  using System;
+  using Microsoft.VisualBasic;
+
+  /// <summary>
+  /// Alerts a user with a GUI InputBox (based on VB library) then passes the information to the console.
+  /// </summary>
+  public sealed class Program
   {
-    public static void Main(string[] args)
+    /// <summary>
+    /// Prevents a default instance of the <see cref="Program"/> class from being created.
+    /// </summary>
+    private Program()
     {
-      string sPrompt = "";
-      string sTitle = "";
-      string sDefault = "";
+    }
+
+    /// <summary>
+    /// Main and single-action.
+    /// </summary>
+    /// <param name="args">arguments from the operation-system.</param>
+    /// <returns>exit-code (0 any arguments were available, or 1 no arguments specified).</returns>
+    public static int Main(string[] args)
+    {
+      string info_prompt = string.Empty;
+      string info_title = string.Empty;
+      string info_default = string.Empty;
       
-      foreach (string arg in args) {
+      foreach (string arg in args) 
+      {
         string tmp = arg.Trim();
         
-        if (tmp.StartsWith("-prompt=", StringComparison.CurrentCultureIgnoreCase))
-          sPrompt = tmp.Replace("-prompt=", "").Replace("\"", "").Replace("\r", "").Replace("\\n", "\n").Replace("\\t", "    ");
-        else if (tmp.StartsWith("-title=", StringComparison.CurrentCultureIgnoreCase))
-          sTitle = tmp.Replace("-title=", "").Replace("\"", "").Replace("\r", "").Replace("\\n", "\n").Replace("\\t", "    ");
+        if (tmp.StartsWith("-prompt=", StringComparison.CurrentCultureIgnoreCase)) 
+        {
+          info_prompt = tmp.Replace("-prompt=", string.Empty).Replace("\"", string.Empty).Replace("\r", string.Empty).Replace("\\n", "\n").Replace("\\t", "    ");
+        }
+        else if (tmp.StartsWith("-title=", StringComparison.CurrentCultureIgnoreCase)) 
+        {
+          info_title = tmp.Replace("-title=", string.Empty).Replace("\"", string.Empty).Replace("\r", string.Empty).Replace("\\n", "\n").Replace("\\t", "    ");
+        } 
         else if (tmp.StartsWith("-default=", StringComparison.CurrentCultureIgnoreCase))
-          sDefault = tmp.Replace("-default=", "").Replace("\"", "").Replace("\r", "").Replace("\\n", "\n").Replace("\\t", "    ");
+        {
+          info_default = tmp.Replace("-default=", string.Empty).Replace("\"", string.Empty).Replace("\r", string.Empty).Replace("\\n", "\n").Replace("\\t", "    ");
+        }
       }
-      if (String.IsNullOrEmpty(sPrompt) && String.IsNullOrEmpty(sTitle) && String.IsNullOrEmpty(sDefault)) {
+
+      if (string.IsNullOrEmpty(info_prompt) && string.IsNullOrEmpty(info_title) && string.IsNullOrEmpty(info_default)) 
+      {
         Console.Error.WriteLine("Error: missing -prompt=\"..\" or -title=\"..\" or -default=\"..\"");
-      } else {
-        Console.WriteLine(
-          Microsoft.VisualBasic.Interaction.InputBox(sPrompt, sTitle, sDefault, -1, -1)
-        );
+        Environment.ExitCode = 1;
+      } 
+      else 
+      {
+        Console.WriteLine(Microsoft.VisualBasic.Interaction.InputBox(info_prompt, info_title, info_default, -1, -1));
+        Environment.ExitCode = 0;
       }
+
+      return Environment.ExitCode;
     }
   }
 }
--- inputbox/Properties/AssemblyInfo.cs
+++ inputbox/Properties/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" author="Elad Karako" company="no">
+//     UnLicensed (Public Domain).
+// </copyright>
+//-----------------------------------------------------------------------
 #region Using directives
 using System;
 using System.Reflection;
@@ -7,22 +12,25 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("inputbox")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("inputbox")]
-[assembly: AssemblyCopyright ("Copyright 2016")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyTitle("InputBox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Elad Karako")]
+[assembly: AssemblyProduct("InputBox")]
+[assembly: AssemblyCopyright("Copyright 2016")]
+[assembly: AssemblyTrademark("UnLicensed (Public Domain)")]
+[assembly: AssemblyCulture("")]
+
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible (true)]
+[assembly: ComVisible(false)]
+
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion ("1.5.3")]
-[assembly: Guid ("6dd374bb-4db4-4776-8e85-2db35b8da7d4")]
+[assembly: AssemblyVersion("1.5.3")]
+[assembly: Guid("6dd374bb-4db4-4776-8e85-2db35b8da7d4")]
+[assembly: CLSCompliant(true)]
